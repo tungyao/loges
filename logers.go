fdb81ab348960b65511fcc332bf94397c103073a
tungyao <1922527051@qq.com>
2020-09-11 11:13:19 +0800

    update

--- logers.go
+++ logers.go
@@ -2,10 +2,12 @@ package loges
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"runtime"
 	"sync"
 	"time"
 )
@@ -39,42 +41,53 @@ type loges struct {
 	urlErrTime chan int
 }
 
-func (l *loges) trace(v ...interface{}) {
+const (
+	BasicAuth = "Basic 123123"
+	EsUrl     = "http://123123"
+)
 
+func (l *loges) trace(v ...interface{}) {
+	byt := []byte(fmt.Sprintln(v))
+	l.send <- byt[1 : len(byt)-2]
 }
 
 func (l *loges) warn(v ...interface{}) {
-
+	byt := []byte(fmt.Sprintln(v))
+	l.send <- byt[1 : len(byt)-2]
 }
 
 func (l *loges) error(v ...interface{}) {
-
+	byt := []byte(fmt.Sprintln(v))
+	l.send <- byt[1 : len(byt)-2]
 }
 
 func (l *loges) fatal(v ...interface{}) {
-
+	byt := []byte(fmt.Sprintln(v))
+	l.send <- byt[1 : len(byt)-2]
 }
 func (l *loges) request(byt []byte) {
 	if !l.urlErr {
 		c := http.Client{}
-		req, err := http.NewRequest("POST", "", bytes.NewReader(byt))
+		req, err := http.NewRequest("POST", EsUrl, bytes.NewReader(byt))
 		if err != nil {
 			l.urlErr = true
 			l.urlErrTime <- 1
 			return
 		}
 		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", BasicAuth)
 		c.Do(req)
 	}
 }
 func (l *loges) hub(filePath string) {
 	// 建立缓冲通道
 	l.send = make(chan []byte, 2048)
+	l.writers = make([]io.Writer, 0)
 	fs, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_RDWR, 766)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	l.writers[0] = fs
+	l.writers = append(l.writers, fs)
 	go func() {
 		for {
 			byt := <-l.send
@@ -100,15 +113,23 @@ func init() {
 }
 
 func Println(v ...interface{}) {
-	defaultLoges.trace(v)
+	pc, file, line, ok := runtime.Caller(1)
+	f := runtime.FuncForPC(pc)
+	defaultLoges.trace("info", time.Now().Format("2006-01-02 15:04:05"), pc, file, line, ok, f.Name(), v)
 }
 
 func Panic(v ...interface{}) {
-	defaultLoges.error(v)
+	pc, file, line, ok := runtime.Caller(1)
+	f := runtime.FuncForPC(pc)
+	defaultLoges.error("error", time.Now().Format("2006-01-02 15:04:05"), pc, file, line, ok, f.Name(), v)
 }
 func Warn(v ...interface{}) {
-	defaultLoges.warn(v)
+	pc, file, line, ok := runtime.Caller(1)
+	f := runtime.FuncForPC(pc)
+	defaultLoges.warn("warn", time.Now().Format("2006-01-02 15:04:05"), pc, file, line, ok, f.Name(), v)
 }
 func Fatal(v ...interface{}) {
-	defaultLoges.fatal(v)
+	pc, file, line, ok := runtime.Caller(1)
+	f := runtime.FuncForPC(pc)
+	defaultLoges.fatal("fatal", time.Now().Format("2006-01-02 15:04:05"), pc, file, line, ok, f.Name(), v)
 }
--- test/loges_test.go
+++ test/loges_test.go
@@ -1,12 +1,27 @@
 package test
 
 import (
+	"errors"
+	"fmt"
+	"reflect"
 	"testing"
 
-	log "../../loges"
+	log "../../loges2"
 )
 
 func TestLoges(t *testing.T) {
 	// log.Error()
-	log.Println("hello")
+	log.Println(errors.New("123"))
+}
+func TestRef(t *testing.T) {
+	v := []byte("123")
+	// v:="123"
+	types := reflect.TypeOf(v)
+	fmt.Println(types.String())
+	switch types.Name() {
+	case "string":
+		fmt.Println(123)
+	case "[]uint8":
+
+	}
 }
